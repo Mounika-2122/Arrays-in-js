dac97096f71000b49d933abeedbaa6e5dd11167f
Mounika <bojjamounika8@gmail.com>
2021-12-15 23:38:56 +0530

    arraysjs

---
+++ Arraysjs.js
@@ -0,0 +1,19 @@
+var array1 = ["one","two","three","four","five",6,7];
+var array2 = new Array("one","two");
+
+var myList=[];//empty array
+var myList=[1,2,3,4,"one"];
+
+array1.push('eight');
+console.log(array1);
+
+array1.unshift("zero");
+console.log(array1);
+
+array1[2]="Array element";
+console.log(array1);
+
+array1.pop();
+console.log(array1);
+console.log(array1.length);
+
